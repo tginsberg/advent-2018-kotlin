d810406516d8eb2c1fc123aa149a196394097ea9
Todd Ginsberg <todd@ginsberg.com>
2018-12-05 07:47:45 -0600

    Day 5 - Alchemical Reduction

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ I am trying to solve these on the day they are posted with clear, idiomatic solu
 |    2    | Inventory Management System                   | [\[Blog Post\]](https://todd.ginsberg.com/post/advent-of-code/2018/day2/) [\[Code\]](https://github.com/tginsberg/advent-2018-kotlin/blob/master/src/main/kotlin/com/ginsberg/advent2018/Day02.kt) [\[AoC\]](http://adventofcode.com/2018/day/2) |
 |    3    | No Matter How You Slice It                    | [\[Blog Post\]](https://todd.ginsberg.com/post/advent-of-code/2018/day3/) [\[Code\]](https://github.com/tginsberg/advent-2018-kotlin/blob/master/src/main/kotlin/com/ginsberg/advent2018/Day03.kt) [\[AoC\]](http://adventofcode.com/2018/day/3) |
 |    4    | Repose Record                                 | [\[Blog Post\]](https://todd.ginsberg.com/post/advent-of-code/2018/day4/) [\[Code\]](https://github.com/tginsberg/advent-2018-kotlin/blob/master/src/main/kotlin/com/ginsberg/advent2018/Day04.kt) [\[AoC\]](http://adventofcode.com/2018/day/4) |
+|    5    | Alchemical Reduction                          | [\[Blog Post\]](https://todd.ginsberg.com/post/advent-of-code/2018/day5/) [\[Code\]](https://github.com/tginsberg/advent-2018-kotlin/blob/master/src/main/kotlin/com/ginsberg/advent2018/Day05.kt) [\[AoC\]](http://adventofcode.com/2018/day/5) |
 
 
 Copyright &copy; 2018 by Todd Ginsberg.
---
+++ src/main/kotlin/com/ginsberg/advent2018/Day05.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2018 by Todd Ginsberg
+ */
+
+/**
+ * Advent of Code 2018, Day 5 - Alchemical Reduction
+ *
+ * Problem Description: http://adventofcode.com/2018/day/5
+ * Blog Post/Commentary: https://todd.ginsberg.com/post/advent-of-code/2018/day5/
+ */
+package com.ginsberg.advent2018
+
+class Day05(private val input: String) {
+
+    fun solvePart1(): Int =
+        input.react().length
+
+    fun solvePart2(): Int =
+        ('A'..'Z')
+            .map { input.remove(it).react().length }
+            .min()
+            ?: throw IllegalStateException()
+
+    private fun String.react(): String =
+        this.fold(mutableListOf<Char>()) { done, char ->
+            when {
+                done.firstOrNull() matches char -> done.removeAt(0)
+                else -> done.add(0, char)
+            }
+            done
+        }
+            .reversed()
+            .joinToString(separator = "")
+
+    private fun String.remove(char: Char): String =
+        this.replace(char.toString(), "", true)
+
+    private infix fun Char?.matches(other: Char): Boolean =
+        when {
+            this == null -> false
+            this.toUpperCase() != other.toUpperCase() -> false
+            this.isUpperCase() && other.isLowerCase() -> true
+            this.isLowerCase() && other.isUpperCase() -> true
+            else -> false
+        }
+
+}
